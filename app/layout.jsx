161d4e36ff5434b449bfa364e448daaa1877035b
quinnn7 <baguette254@gmail.com>
2025-12-30 15:18:47 -0700

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "365 Bible in a Year",
+  description: "Read the Bible in a year"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
